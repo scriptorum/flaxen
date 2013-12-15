d325591e539b7e3d1155e55b13d03160b86bcb33
Eric W. Lund <github@egotron.com>
2013-12-15 10:27:55 -0500

    Added static constructors for Alpha.

--- source/flaxen/component/Alpha.hx
+++ source/flaxen/component/Alpha.hx
@@ -8,4 +8,19 @@ class Alpha
 	{
 		this.value = value;
 	}
+
+	public static function clear()
+	{
+		return new Alpha(0.0);
+	}
+
+	public static function opaque()
+	{
+		return new Alpha(1.0);
+	}
+
+	public static function half()
+	{
+		return new Alpha(1.0);
+	}
 }
