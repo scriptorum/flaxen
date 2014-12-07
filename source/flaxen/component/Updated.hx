24906f4a571aee2ed1157e501b4693a89ad7d8c1
Eric W. Lund <github@egotron.com>
2014-12-07 03:51:09 -0500

    Added Updated component.

---
+++ source/flaxen/component/Updated.hx
@@ -0,0 +1,8 @@
+package flaxen.component;
+
+import flaxen.component.StaticComponent;
+
+/**
+	Can be helpful to through this into an entity to let your systems know they been updated.
+ */
+class Updated extends StaticComponent { }
